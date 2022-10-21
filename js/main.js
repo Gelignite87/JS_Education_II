e0a16ea5133210a46974e97d4b1fb41271088c2d
Gelignite87 <railgun87@rambler.ru>
2022-10-22 09:05:42 +1300

    22.10.2022

--- js/main.js
+++ js/main.js
@@ -26,15 +26,6 @@ class ProductList {
       block.insertAdjacentHTML('beforeend', productObject.render());
     }
   }
-  // priceAllProduct() {
-  //   let priceAllProduct = 0;
-  //   const block = document.querySelector(this.container);
-  //   for (let product of this.allProducts) {
-  //     const { id, title, img, price } = product;
-  //     priceAllProduct += price;
-  //   }
-  //   block.insertAdjacentHTML('afterend', `<p style="font-size: 25px; padding-left: 40px;">Суммарная стоимость товаров: ${priceAllProduct} \u20bd</p>`);
-  // }
 }
 
 class ProductItem {
@@ -57,46 +48,3 @@ class ProductItem {
 }
 
 new ProductList();
-
-/**-------------------------------------------------------*/
-class Basket {
-  constructor(X) {
-    this.allProducts = [];
-  }
-  render() {
-  }
-}
-
-class BasketElements {
-  constructor({ title = 'name', price = 0, id = 0 }) {
-    this.title = title;
-    this.price = price;
-    this.id = id;
-  }
-  render() {
-  }
-}
-/**-------------------------------------------------------*/
-
-// const products = [
-//   {id: 1, title: 'Notebook', price: 20000},
-//   {id: 2, title: 'Mouse', price: 1500},
-//   {id: 3, title: 'Keyboard', price: 5000},
-//   {id: 4, title: 'Gamepad', price: 4500},
-// ];
-//
-// const renderProduct = (item, img='https://placehold.it/200x150') => `<div class="product-item" data-id="${this.id}">
-//               <img src="${img}" alt="Some img">
-//               <div class="desc">
-//                   <h3>${item.title}</h3>
-//                   <p>${item.price} \u20bd</p>
-//                   <button class="buy-btn">Купить</button> 
-//               </div>
-//           </div>`;
-//
-// const renderProducts = list => {
-// document.querySelector('.products').insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
-// };
-//
-// renderProducts(products);
-//
